46ca944167c9f9a403b0744083f1ce36fd6640e6
ChKran <mai17030@uom.edu.gr>
2017-01-18 13:32:21 +0200

    First roomlistview

---
+++ src/RoomListView.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+import javax.swing.*;
+/*
+ * Created by JFormDesigner on Wed Jan 18 13:19:06 EET 2017
+ */
+
+
+
+/**
+ * @author babis naskre
+ */
+public class RoomListView extends JFrame {
+	public RoomListView() {
+		initComponents();
+	}
+
+	private void initComponents() {
+		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+		// Generated using JFormDesigner Evaluation license - babis naskre
+
+		//======== this ========
+		Container contentPane = getContentPane();
+		contentPane.setLayout(null);
+
+		{ // compute preferred size
+			Dimension preferredSize = new Dimension();
+			for(int i = 0; i < contentPane.getComponentCount(); i++) {
+				Rectangle bounds = contentPane.getComponent(i).getBounds();
+				preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+				preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+			}
+			Insets insets = contentPane.getInsets();
+			preferredSize.width += insets.right;
+			preferredSize.height += insets.bottom;
+			contentPane.setMinimumSize(preferredSize);
+			contentPane.setPreferredSize(preferredSize);
+		}
+		pack();
+		setLocationRelativeTo(getOwner());
+		// JFormDesigner - End of component initialization  //GEN-END:initComponents
+	}
+
+	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+	// Generated using JFormDesigner Evaluation license - babis naskre
+	// JFormDesigner - End of variables declaration  //GEN-END:variables
+}
